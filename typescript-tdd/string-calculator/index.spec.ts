cf173e954b858c704deafe83b1b8f1b489fa236c
Thomas Monte <thomasmont3@gmail.com>
2021-05-10 11:35:09 -0600

    add string calculator solution

--- typescript-tdd/string-calculator/index.spec.ts
+++ typescript-tdd/string-calculator/index.spec.ts
@@ -1,15 +1,35 @@
 import "jasmine";
 import { add } from "./index";
 
-describe("Summing up strings", () => {
-  const sums: Array<[string, number]> = [
+describe("String calculator", () => {
+  const values: Array<[string, number]> = [
     ["", 0],
     ["1", 1],
+    ["1,2", 3],
+    ["1,2,3", 6],
+    ["1,2\n3", 6],
+    ["1,2,1000", 3],
+    ["1,1001,1002", 1],
+    ["//;\n1;2", 3],
+    ["//[***]\n1***2", 3],
+    ["//[***][;;;]\n1***2;;;4", 7],
   ];
 
-  sums.forEach(([sum, result]) => {
-    it(`for sum ${sum} should return ${result}`, () => {
-      expect(add(sum)).toBe(result);
+  values.forEach(([value, result]) => {
+    it(`for sum ${value} should return ${result}`, () => {
+      expect(add(value)).toBe(result);
     });
+  });
+
+  it(`for sum 1,-2 should throw`, () => {
+    expect(() => add("1,-2")).toThrowError(
+      "Negative numbers are not supported: -2"
+    );
+  });
+
+  it(`for sum 1,-2,-3 should throw`, () => {
+    expect(() => add("1,-2,-3")).toThrowError(
+      "Negative numbers are not supported: -2,-3"
+    );
   });
 });
--- typescript-tdd/string-calculator/index.ts
+++ typescript-tdd/string-calculator/index.ts
@@ -1,3 +1,53 @@
-export function add(sum: string): number {
-  return 0;
+function sum(values: string[]) {
+  return values
+    .map((v) => parseInt(v))
+    .filter((n) => n < 1000)
+    .reduce((acc, val) => {
+      acc += val;
+      return acc;
+    }, 0);
+}
+
+function getSumDefinition(value: string) {
+  if (value.startsWith("//")) {
+    return value.substring(value.indexOf("\n") + 1);
+  } else {
+    return value;
+  }
+}
+
+function getDelimiterExpression(value: string) {
+  if (!value.startsWith("//")) {
+    return /,|\n/g;
+  }
+  const delimiterDefinition = value.substring(
+    value.lastIndexOf("//") + 2,
+    value.indexOf("\n")
+  );
+  const delimiters = value.startsWith("//[")
+    ? delimiterDefinition.split(/[\[\]]/).filter((v) => !!v)
+    : [delimiterDefinition];
+  const expr = `,|\\n|${delimiters
+    .map((delimiter) =>
+      delimiter
+        .split("")
+        .map((v) => (v === "*" ? `\\${v}` : v))
+        .join("")
+    )
+    .join("|")}`;
+  return new RegExp(expr);
+}
+
+export function add(value: string): number {
+  if (!value) return 0;
+  const sumDefinition = getSumDefinition(value);
+  const regex = getDelimiterExpression(value);
+  const values = sumDefinition.split(regex);
+  const negatives = values.filter((value) => value.startsWith("-"));
+  if (negatives.length) {
+    throw new Error(
+      `Negative numbers are not supported: ${negatives.join(",")}`
+    );
+  }
+  return sum(values);
 }
